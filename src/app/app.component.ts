5816d09ed3b99f58237ce2b66d1600703e61b7a5
Terry Kinder <terry.kinder@gmail.com>
2021-11-08 21:00:19 -0600

    updated due to changes being lost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'crypto-two';
+  objectKeys = Object.keys;
+  cryptos: any;
+  interval: any;
+
+  constructor(private _data: DataService) {}
+
+  ngOnInit() {
+    this.refreshData();
+    this.interval = setInterval(() => {
+      this.refreshData();
+    }, 30000);
+  }
+
+  refreshData() {
+    this._data.getPrices().subscribe((res) => {
+      this.cryptos = res;
+      console.log(res);
+    });
+  }
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators'
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  result: any;
+
+  constructor(private _http: HttpClient) {}
+
+
+  getPrices() {
+    return this._http
+      .get(
+        'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD&api_key=24bd80c24960d3cf31733291331a298ae53520fc2bfdb3910451fbf0cf021902'
+      )
+      .pipe(map((result: any) => (this.result = result)));
+  }
+}
+
