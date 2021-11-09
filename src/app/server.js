c733c0ae3eab8af415d846134d78cd4c42f69059
Terry Kinder <terry.kinder@gmail.com>
2021-11-08 22:15:59 -0600

    corrected error in server.js express

--- src/app/server.js
+++ src/app/server.js
@@ -1,14 +1,14 @@
 //Install express server
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/crypto-two"));
+app.use(express.static('./dist/crypto-two'));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/crypto-two/" })
+  res.sendFile("index.html", { root: 'dist/crypto-two/' })
 );
 
 // Start the app by listening on the default Heroku port
