4aa028e1e4f258e1e41ffa76eb632a3ae187962a
Terry Kinder <terry.kinder@gmail.com>
2022-01-18 19:47:34 -0600

    Added CORS and app.use(cors()); to server.js

--- server.js
+++ server.js
@@ -11,5 +11,7 @@ app.get("/*", (req, res) =>
   res.sendFile("index.html", { root: "dist/crypto-two/" })
 );
 
+app.use(cors());
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
