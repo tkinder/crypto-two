eeb37a1882f3099c792dcc918b5534478927f12c
Terry Kinder <terry.kinder@gmail.com>
2022-01-19 00:03:04 -0600

    Additional angular.json and proxy.conf.json changes

--- src/app/server.js
+++ src/app/server.js
@@ -9,25 +9,9 @@
 // app.use(cors(corsOptions)) // Use this after the variable declaration
 
 const express = require('express');
-var cors = require("cors");
 const path = require('path');
 
 const app = express();
-
-app.options('*', cors()) // include before other routes
-
-var whitelist = [
-  "https://newsapi.org/v2/everything?q=bitcoin&apiKey=",
-];
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions); // callback expects two parameters: error and options
-};
 
 // Serve only the static files form the dist directory
 app.use(express.static("./dist/angular-app-name"));
@@ -37,6 +21,4 @@ app.get("/*", (req, res) =>
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, function() {
-  console.log('CORS-enabled web server listening on port 8080')
-});
+app.listen(process.env.PORT || 8080);
