29de756281a5a71e50984dfec51c391f036379d1
Terry Kinder <terry.kinder@gmail.com>
2022-01-18 22:12:49 -0600

    Changes to server.js

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 //Install express server
+const cors=require("cors");
+const corsOptions ={
+   origin:'*',
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 const express = require('express');
 const path = require('path');
 
