9777072f8921abafa05b2a9c84a2a43c30812c26
Terry Kinder <terry.kinder@gmail.com>
2022-01-20 15:37:23 -0600

    Changes to server.js

--- src/app/server.js
+++ src/app/server.js
@@ -5,7 +5,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/crypto-two"));
+app.use(express.static("./dist/angular-app-name"));
+
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/angular-app-name/" })
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
