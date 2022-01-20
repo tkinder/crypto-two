68ab8aea5fa151e716f0f593833a62b6534acb9e
Terry Kinder <terry.kinder@gmail.com>
2022-01-20 09:06:08 -0600

    Uncomments

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,20 +1,20 @@
-// import { HttpClient } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
-// import { feed } from './Feed.component';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { feed } from './Feed.component';
 
 
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class RestService {
+@Injectable({
+  providedIn: 'root',
+})
+export class RestService {
 
-//   constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
 
-// url: string = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+url: string = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
 
-// getFeed()
-// {
-//   return this.http.get.<feed[]>(this.url);
-//   }
-// }
+getFeed()
+{
+  return this.http.get.<feed[]>(this.url);
+  }
+}
