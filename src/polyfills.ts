802ae6fdc0490d1352a9475c483c42f61d7d5d27
Terry Kinder <terry.kinder@gmail.com>
2022-01-20 13:45:19 -0600

    More updates

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,5 @@
+(window as any).__Zone_disable_navigator.userAgentData = true;
+
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -61,5 +63,4 @@ import 'zone.js';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
- * APPLICATION IMPORTS
  */
